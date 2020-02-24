c7260d3a4619660d06604710cfe817c90d06189e
Andrew Brenner <Abrenner@wifi-roaming-128-4-141-221.host.udel.edu>
2020-02-24 14:26:00 -0500

    ships from images to sprites now (animated)

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,11 +1,12 @@
 import ExampleObject from '../objects/exampleObject';
+import { timingSafeEqual } from 'crypto';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
   background: Phaser.GameObjects.TileSprite;
-  ship1: Phaser.GameObjects.Image;
-  ship2: Phaser.GameObjects.Image;
-  ship3: Phaser.GameObjects.Image;
+  ship1: Phaser.GameObjects.Sprite;
+  ship2: Phaser.GameObjects.Sprite;
+  ship3: Phaser.GameObjects.Sprite;
 
 
   constructor() {
@@ -20,23 +21,56 @@ export default class MainScene extends Phaser.Scene {
     this.background.setOrigin(0,0);
 
     this.make_ships();
-
+    /*
     this.add.text(20,20,"Hellooooo world (^ ^)b", {
       font: "20px Arial", 
       fill: "blue"
     });
+    */
 
 
   }
 
   make_ships(){
-    this.ship1 = this.add.image(this.scale.width/2 - 50, this.scale.height/2, "ship");
-    this.ship2 = this.add.image(this.scale.width/2, this.scale.height/2, "ship2");
-    this.ship3 = this.add.image(this.scale.width/2 + 50, this.scale.height/2, "ship3");
+    this.ship1 = this.add.sprite(this.scale.width/2 - 50, this.scale.height/2, "ship");
+    this.ship2 = this.add.sprite(this.scale.width/2, this.scale.height/2, "ship2");
+    this.ship3 = this.add.sprite(this.scale.width/2 + 50, this.scale.height/2, "ship3");
 
-    this.ship2.flipY = true;
-    this.ship1.setScale(2);
-    this.ship3.setScale(2);
+    this.anims.create({
+      key: "ship1_anim", // creating animation called ship1_anim
+      frames: this.anims.generateFrameNumbers("ship"), // use spritesheet ship
+      frameRate: 20, // 20 frames per second
+      repeat: -1 // infinite loop
+    });
+    this.anims.create({
+      key: "ship2_anim", // creating animation called ship1_anim
+      frames: this.anims.generateFrameNumbers("ship2"), // use spritesheet ship
+      frameRate: 20, // 20 frames per second
+      repeat: -1 // infinite loop
+    });
+    this.anims.create({
+      key: "ship3_anim", // creating animation called ship1_anim
+      frames: this.anims.generateFrameNumbers("ship3"), // use spritesheet ship
+      frameRate: 20, // 20 frames per second
+      repeat: -1 // infinite loop
+    });
+    this.anims.create({
+      key: "explosion_anim", // creating animation called ship1_anim
+      frames: this.anims.generateFrameNumbers("explosion"), // use spritesheet ship
+      frameRate: 20, // 20 frames per second
+      repeat: 0, // infinite loop
+      hideOnComplete: true
+    });
+
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+
+
+
+    //this.ship2.flipY = true;
+    //this.ship1.setScale(2);
+    //this.ship3.setScale(2);
   }
 
   move_ship(ship, speed){
@@ -51,14 +85,15 @@ export default class MainScene extends Phaser.Scene {
 
   update() {
 
-    this.ship1.angle += 3;
-    this.ship3.angle -= 3;
-
+    //this.ship1.angle += 3;
+    //this.ship3.angle -= 3;
+/*
     if(this.ship2.y >= 0){
       this.move_ship(this.ship2, 3);
     } else {
       this.resetShipPos(this.ship2)
     }
+  */
 
     this.background.tilePositionY -= 0.5;
 
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,10 +4,9 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("background", "assets/images/background.png");
-    this.load.image("ship", "assets/images/ship.png");
-    this.load.image("ship2", "assets/images/ship2.png");
-    this.load.image("ship3", "assets/images/ship3.png");
+    this.load_images();
+    this.load_spritesheets();
+
 
 
 
@@ -19,4 +18,37 @@ export default class PreloadScene extends Phaser.Scene {
 
     this.scene.start('MainScene');
   }
+
+
+  load_images(){
+    this.load.image("background", "assets/images/background.png");
+    // loading ship images as spritesheets now
+    //this.load.image("ship", "assets/images/ship.png");
+    //this.load.image("ship2", "assets/images/ship2.png");
+    //this.load.image("ship3", "assets/images/ship3.png");
+  }
+
+  load_spritesheets(){
+    this.load.spritesheet("ship", "assets/spritesheets/ship.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+
+    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16
+    });
+    
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png", {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+
+  }
 }
+
